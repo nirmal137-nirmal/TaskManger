131483e2b336141c266929a3b14ad4660e8097eb
Nirmal Fayake <145054254+nirmal137-nirmal@users.noreply.github.com>
2026-04-15 16:43:23 +0530

    Delete TaskManager/TaskManager directory

--- TaskManager/TaskManager/src/main/java/com/example/TaskManager/model/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.TaskManager.model;
-
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- TaskManager/TaskManager/src/main/java/com/example/TaskManager/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.TaskManager.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.TaskManager.model.Task;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
